640c5145451756f3bf5cf19932281ac651fe9312
THIBAULT OBER <SESA432188@schneider-electric.com>
2016-08-03 08:15:12 +0000

    update test

--- threadpool_test.py
+++ threadpool_test.py
@@ -22,7 +22,7 @@ class ConsumerThread(Thread):
         while True:
             val = self.queue.get(block=True)
             self.pool.enqueue(self.read,val)
-            #remove comment to see 
+            #remove comment to see first threadpool working
             #time.sleep(0.2)
             
     def read(self,val):
